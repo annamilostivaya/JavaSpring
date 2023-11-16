dcc115733a8537ef76b825d9d8361eabd56c3bc7
Anna <94928985+annamilostivaya@users.noreply.github.com>
2023-11-16 10:32:09 +0200

    Add files via upload

---
+++ Lab19/SpringBootGroovy/src/main/java/org/o7planning/freemarker/SpringBootGroovyApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGroovyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGroovyApplication.class, args);
+	}
+
+}
