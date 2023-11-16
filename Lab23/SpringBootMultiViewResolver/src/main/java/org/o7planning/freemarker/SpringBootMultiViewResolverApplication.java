3ad910c0f671f146955349c308dda72a53189ce7
Anna <94928985+annamilostivaya@users.noreply.github.com>
2023-11-16 10:38:29 +0200

    Add files via upload

---
+++ Lab23/SpringBootMultiViewResolver/src/main/java/org/o7planning/freemarker/SpringBootMultiViewResolverApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiViewResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultiViewResolverApplication.class, args);
+	}
+
+}
---
+++ Lab24/SpringBootBootstrap/src/test/java/org/o7planning/freemarker/SpringBootBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.freemarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
