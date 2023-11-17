385b0e6197b2e5b70480810e4003ac1b94521a07
Anna <94928985+annamilostivaya@users.noreply.github.com>
2023-11-17 14:58:53 +0200

    Delete Lab27 directory

--- Lab27/SpringBootJPAMultiDS/src/main/java/org/o7planning/sbjpamultids/config/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.o7planning.sbjpamultids.config;
-public class Constants {
-    public static final String PACKAGE_ENTITIES_1 = "org.o7planning.sbjpamultids.entity1";
-    public static final String PACKAGE_ENTITIES_2 = "org.o7planning.sbjpamultids.entity2";
-    public static final String JPA_UNIT_NAME_1 ="PERSITENCE_UNIT_NAME_1";
-    public static final String JPA_UNIT_NAME_2 ="PERSITENCE_UNIT_NAME_2";
-}
