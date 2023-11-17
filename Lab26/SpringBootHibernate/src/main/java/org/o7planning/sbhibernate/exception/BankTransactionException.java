5e95d1911ef9728aed1fa6309a1d7746744edbc9
Anna <94928985+annamilostivaya@users.noreply.github.com>
2023-11-17 14:59:15 +0200

    Delete Lab26 directory

--- Lab26/SpringBootHibernate/src/main/java/org/o7planning/sbhibernate/exception/BankTransactionException.java
+++
@@ -1,9 +0,0 @@
-package org.o7planning.sbhibernate.exception;
-
-public class BankTransactionException extends Exception {
-    private static final long serialVersionUID = -3128681006635769411L;
-
-    public BankTransactionException(String message) {
-        super(message);
-    }
-}
